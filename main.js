39f2514d271c4b8eef0f54cfa8915e7e4f706cf2
Abdelrahman-dev3 <abdoahmed20102005@gmail.com>
2025-01-27 09:08:37 +0200

    shosesShop

---
+++ main.js
@@ -0,0 +1,43 @@
+let mazlomon = document.getElementById("mazlomon")
+mazlomon.onclick = function () {
+    dadalertid.style.display = "block"
+}
+
+let closebtn = document.getElementById("closebtn")
+let dadalertid = document.getElementById("dadalertid")
+
+closebtn.addEventListener("click", function () {
+    dadalertid.style.display = "none"
+
+}
+)
+
+
+
+let buttoms = document.querySelectorAll("button[id=clickright]")
+let buttomsl = document.querySelectorAll("button[id=clickleft]")
+let shiftValue = 237;
+buttoms.forEach((ele) => {
+    ele.addEventListener('click', function () {
+        let param = ele.closest('.sportproduct'); 
+                let sssad = param.lastElementChild
+                param.scrollBy({left: shiftValue,behavior: "smooth"});
+             let currentLeft = parseInt(window.getComputedStyle(ele).left, 10) || 0; //237
+            let currentRight = parseInt(window.getComputedStyle(sssad).left, 10) || 0;
+            let paramWidth = param.scrollWidth;
+            let containerWidth = param.clientWidth; 
+            if (param.scrollLeft + containerWidth >= paramWidth) {return;}
+            ele.style.left = `${currentLeft + shiftValue}px`; 
+            sssad.style.left = `${currentRight + shiftValue}px`; 
+            });});
+buttomsl.forEach((ele) => {
+    ele.addEventListener('click', function () {
+        let param = ele.closest('.sportproduct'); 
+            let sssad = param.firstElementChild
+            param.scrollBy({left: -shiftValue,behavior: "smooth"});
+            let currentRight = parseInt(window.getComputedStyle(sssad).left, 10) || 0;
+            let currentLeft = parseInt(window.getComputedStyle(ele).left, 10) || 0;
+            if (currentLeft === 0) {return; }
+            sssad.style.left = `${currentRight - shiftValue}px`; 
+            ele.style.left = `${currentLeft - shiftValue}px`; 
+        })});
